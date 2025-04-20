4d6bad5cb01a4b37cb8230608fb4917ca7ffc228
hatic <hatic@DESKTOP-JP7S5K6>
2025-04-21 02:46:54 +0300

    Giriş ekranı tasarımı

--- cafe_yonetim_sistemi/Program.cs
+++ cafe_yonetim_sistemi/Program.cs
@@ -16,7 +16,7 @@ namespace cafe_yonetim_sistemi
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
         }
     }
 }
